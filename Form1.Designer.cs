f9022b430f02d7f1b650b48c574366f9de3a7dbf
lksin <2517871772@qq.com>
2024-09-23 13:29:29 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(81, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MatrixA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(295, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MatrixB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(520, 67);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Results";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(462, 119);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(207, 274);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 119);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox2.Size = new System.Drawing.Size(105, 274);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(287, 119);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox3.Size = new System.Drawing.Size(110, 274);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(191, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(191, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "x";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(191, 278);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "转置";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(191, 322);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 57);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "求逆 矩阵A";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(420, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 494);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //add
+        {
+
+        }
+    }
+    public class Matrix
+    {
+        private double[,] data;
+
+        public Matrix(double[,] data) //构造函数
+        {
+            this.data = data;
+        }
+        public int Rows //行数
+        {
+            get { return data.GetLength(0); }
+        }
+        public int Columns //列数
+        {
+            get { return data.GetLength(1); }
+        }
+        public Matrix Add(Matrix other) //加法
+        {
+            if (this.Rows != other.Rows || this.Columns != other.Columns)
+            {
+                throw new ArgumentException("两个矩阵不匹配");
+            }
+            double[,] results = new double[this.Rows, this.Columns];
+            for (int i = 0; i < this.Rows; i++)
+            {
+                for (int j = 0; j < this.Columns; j++)
+                {
+                    results[i, j] = this.data[i, j] + other.data[i, j];
+                }
+            }
+            return new Matrix(results);
+        }
+        public Matrix Sub(Matrix other) //减法
+        {
+            if (this.Rows != other.Rows || this.Columns != other.Columns)
+            {
+                throw new ArgumentException("两个矩阵不匹配");
+            }
+            double[,] results = new double[this.Rows, this.Columns];
+            for (int i = 0; i < this.Rows; i++)
+            {
+                for (int j = 0; j < this.Columns; j++)
+                {
+                    results[i, j] = this.data[i, j] - other.data[i, j];
+                }
+            }
+            return new Matrix(results);
+        }
+        public Matrix Mul(Matrix other) //乘法
+        {
+            if (this.Columns != other.Rows) //矩阵乘法的条件
+            {
+                throw new ArgumentException("两个矩阵不匹配");
+            }
+            double[,] results = new double[this.Rows, other.Columns];
+            for (int i = 0; i < this.Rows; i++)
+            {
+                for (int j = 0; j < other.Columns; j++)
+                {
+                    results[i, j] = 0;
+                    for (int k = 0; k < this.Columns; k++)
+                    {
+                        results[i, j] += this.data[i, k] * other.data[k, j];
+                    }
+                }
+            }
+            return new Matrix(results);
+        }
+        public Matrix Trans() //转置
+        {
+            double[,] results = new double[this.Columns, this.Rows];
+            for (int i = 0; i < this.Rows; i++)
+            {
+                for (int j = 0; j < this.Columns; j++)
+                {
+                    results[j, i] = this.data[i, j];
+                }
+            }
+            return new Matrix(results);
+        }
+        public Matrix Inverse() //求逆
+        {
+            if (this.Rows != 2 || this.Columns != 2)
+            {
+                throw new ArgumentException("矩阵不是2*2");
+            }
+            double det = this.data[0, 0] * this.data[1, 1] - this.data[0, 1] * this.data[1, 0]; //求行列式
+            if (det == 0)
+            {
+                throw new ArgumentException("矩阵不可逆");
+            }
+            double[,] results = new double[2, 2];
+            results[0, 0] = this.data[1, 1] / det;
+            results[1, 1] = this.data[0, 0] / det;
+            results[0, 1] = -this.data[0, 1] / det;
+            results[1, 0] = -this.data[1, 0] / det;
+
+            return new Matrix(results);
+        }
+        public void Print_Martrix() //输出
+        {
+            for (int i = 0; i < this.Rows; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < this.Columns; j++)
+                {
+                    Console.Write(this.data[i, j] + " ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static double[] Read(string input)
+        {
+            Regn regn = new Regn();
+            double[,] matrix;
+            string[] str = input.Split(' ');
+            for(int i)
+        }
+    }
+    public class Regn //判断输入是否为数字
+    {
+        public bool regn(string input)
+        {
+            Regex reg = new Regex("^-?\\d+$|^(-?\\d+)(\\.\\d+)?$");
+            return reg.IsMatch(input);
+        }
+    }
+}
